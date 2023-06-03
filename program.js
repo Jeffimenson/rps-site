99bfa4432254f7478f3cdcb2a4822cc7894f1dfe
Jeffimenson <jeffengim@gmail.com>
2023-06-03 18:03:11 -0400

    Encase stuff into functions and add round counter

--- program.js
+++ program.js
@@ -1,12 +1,26 @@
-let wantsToPlay = true; 
-while (wantsToPlay){
-    let playerChoice = getPlayerChoice(); 
-    let computerChoice = getComputerChoice(); 
-    while (playerChoice === null){ 
-        alert("That's not an option! >:("); 
-        playerChoice = getPlayerChoice(); 
+game(); 
+
+function game(){
+    let rounds = 0; 
+    let wins = 0; 
+
+    while (true){
+        let playerChoice = getPlayerChoice(); 
+        let computerChoice = getComputerChoice(); 
+        
+        while (playerChoice === ""){ 
+            alert("That's not an option! >:("); 
+            playerChoice = getPlayerChoice(); 
+        }
+        if (playerChoice === null){
+            alert(`Ending game. You won ${wins} out of ${rounds} rounds.`); 
+            break; 
+        }
+        rounds++; 
+        if (playRound(playerChoice, computerChoice)){
+            wins++; 
+        }
     }
-    playRound(playerChoice, computerChoice); 
 }
 
 function playRound(playerChoice, computerChoice){
@@ -16,20 +30,24 @@ function playRound(playerChoice, computerChoice){
         let playerWon = determineWin(playerChoice, computerChoice); 
         if (playerWon){
             alert(`${playerChoice} beats ${computerChoice} - you won!`); 
+            return true; 
         } else {
             alert(`${playerChoice} loses to ${computerChoice}... you lost! :(`); 
+            return false; 
         }
     }
 }
 
-
 function getPlayerChoice(){
     let input = prompt("What do you choose - rock, paper, or scissors?"); 
-    input = input.toUpperCase().trim(); 
+    if (input === null)
+        return null; 
+    input = input.toUpperCase(); 
+    input = input.trim(); 
     if (input === "ROCK" || input === "PAPER" || input === "SCISSORS"){
         return input;
     } else {
-        return null; 
+        return ""; 
     }
 }
 
